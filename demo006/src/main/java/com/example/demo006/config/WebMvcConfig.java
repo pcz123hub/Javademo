46795ec13630af06e3830890c9f28914739d2a2d
pcz123hub <3434426338@qq.com>
2025-03-20 17:58:32 +0800

    更新

--- demo006/src/main/java/com/example/demo006/config/WebMvcConfig.java
+++ demo006/src/main/java/com/example/demo006/config/WebMvcConfig.java
@@ -2,7 +2,6 @@ package com.example.demo006.config;
 
 //import com.example.demo006.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
